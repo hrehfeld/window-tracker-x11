81e81a97f85a63a3fb8b680b2af1f085866fc8cb
hrehfeld <hauke@haukerehfeld.de>
2018-01-04 15:03:16 +0100

    fix: license

--- setup.py
+++ setup.py
@@ -89,11 +89,11 @@ setup(
     # },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license='AGPLv3',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
