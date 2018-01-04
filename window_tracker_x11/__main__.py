a603ebbe29fb702dbc1d5772a116c4af16a8c3ac
hrehfeld <hauke@haukerehfeld.de>
2018-01-04 12:54:26 +0100

    fix: remove license left over from example code

--- window_tracker_x11/__main__.py
+++ window_tracker_x11/__main__.py
@@ -1,32 +1,4 @@
 #!/usr/bin/python
-#
-# examples/record_demo.py -- demonstrate record extension
-#
-#    Copyright (C) 2006 Alex Badea <vamposdecampos@gmail.com>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License
-# as published by the Free Software Foundation; either version 2.1
-# of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
-#    Free Software Foundation, Inc.,
-#    59 Temple Place,
-#    Suite 330,
-#    Boston, MA 02111-1307 USA
-
-'''
-Simple demo for the RECORD extension
-
-Not very much unlike the xmacrorec2 program in the xmacro package.
-'''
-
 
 # Python 2/3 compatibility.
 from __future__ import print_function
